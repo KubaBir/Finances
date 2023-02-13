02782932622c42af7bfae50bd47682999153ef27
Kuba Birecki <kubabir@outlook.com>
2023-02-13 01:07:09 +0100

    User model, docker setup

---
+++ app/core/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None, **kwargs):
+        if not username:
+            raise ValueError('User must have a username')
+        user = self.model(
+            username=username,
+            **kwargs,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None):
+        user = self.create_user(username, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+    nordigen_user_id = models.CharField(max_length=255, null=True, blank=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'username'
+
+    objects = UserManager()
