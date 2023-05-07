8688f7a8721d6af98769475a9c1bfd168c9f73ef
Kuba Birecki <kubabir@outlook.com>
2023-05-07 21:14:34 +0200

    update indicators

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,5 @@
 from core.views import Transaction
+from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 
@@ -10,11 +11,19 @@ class UpdateTransactionStatusView(RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     lookup_field = 'id'
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        report = instance.report
+        self.perform_destroy(instance)
+        report.sync()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class CreateCustomTransactionView(CreateAPIView):
     serializer_class = CustomTransactionSerializer
     queryset = Transaction.objects.all()
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user,
-                        booking_date=self.request.data['value_date'])
+        obj = serializer.save(user=self.request.user,
+                              booking_date=self.request.data['value_date'])
+        obj.report.sync()
